d0d00e02d873cb0d15966ded26eb194e687ab48b
gustavorosa741 <zegu11@hotmail.com>
2025-08-18 16:18:08 -0300

    ajustes script

--- criar_BD.py
+++ criar_BD.py
@@ -13,17 +13,11 @@ load_dotenv()
 usuario = os.getenv("USUARIO")
 senha = os.getenv("SENHA")
 
-engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@localhost:3306/teste")
+engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@localhost:3306/chamados")
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
-
-class StatusChamado(PythonEnum):
-    ABERTO = "Aberto"
-    EM_ANDAMENTO = "Em andamento"
-    AGUARDANDO_PECAS = "Aguardando peças"
-    CONCLUIDO = "Concluído"
 
 class Chamado(Base):
     __tablename__ = 'chamado'
@@ -31,16 +25,12 @@ class Chamado(Base):
     id = Column(Integer, primary_key=True)
     id_funcionario = Column(Integer, ForeignKey('usuario.id'))
     id_maquina = Column(Integer, ForeignKey('maquina.id'))
-    categoria = Column(String(200))
+    categoria = Column(Integer, ForeignKey('categoria_chamado.id'), nullable=False)
     data_abertura = Column(Date, nullable=False)
     data_fechamento = Column(Date)
     problema = Column(Text, nullable=False)
     solucao = Column(Text)
-    progresso = Column(Enum(StatusChamado), default=StatusChamado.ABERTO)
-    
-    # Relacionamentos
-    funcionario = relationship("Usuario", back_populates="chamados")
-    maquina = relationship("Maquina", back_populates="chamados")
+    progresso = Column(String(200), nullable=False)
 
 class Maquina(Base):
     __tablename__ = 'maquina'
@@ -48,24 +38,27 @@ class Maquina(Base):
     id = Column(Integer, primary_key=True)
     nome_maquina = Column(String(200), nullable=False)
     setor = Column(String(200), nullable=False)
-    
-    # Relacionamento
-    chamados = relationship("Chamado", back_populates="maquina")
 
 class Usuario(Base):
     __tablename__ = 'usuario'
     
     id = Column(Integer, primary_key=True)
-    nome = Column(String(100), nullable=False)
+    nome = Column(String(200), nullable=False)
     usuario = Column(String(50), unique=True, nullable=False)
     senha = Column(String(200), nullable=False)
     nivel_acesso = Column(Integer, default=1)
     
-    # Relacionamento
-    chamados = relationship("Chamado", back_populates="funcionario")
     
     def set_senha(self, senha):
         self.senha = generate_password_hash(senha)
+
+class CategoriaChamado(Base):
+    __tablename__ = 'categoria_chamado'
+    
+    id = Column(Integer, primary_key=True)
+    categoria = Column(String(200), nullable=False)
+
+
 
 def create_tables():
     Base.metadata.create_all(engine)
